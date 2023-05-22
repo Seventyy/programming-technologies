b7093ee7059bb8f4af386c3858f16076b336c0e4
Seventyy <seventyyyyy@gmail.com>
2023-05-22 20:40:27 +0200

    Rough implementation of not working anything

---
+++ PresentationLayer/Model/ICustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Model
+{
+    internal interface ICustomer
+    {
+        int CustomerId { get; }
+        string First_Name { get; set; }
+        int Last_Name { get; set; }
+        void Add(int cid, string fn, string ln);
+        void Delete(int cid);
+        void Update(int cid, int new_cid, string fn, string ln);
+    }
+}
