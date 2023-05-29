8a79b8fb563b88431c33e0472f3df5f66becbd8a
JJacob <jjanicki08@gmail.com>
2023-05-29 18:51:27 +0200

    service tests

--- ServiceTests/UnitTest1.cs
+++ ServiceTests/UnitTest1.cs
@@ -8,31 +8,32 @@ namespace ServiceTests
         [TestMethod]
         public void TestMethod1()
         {
-            IServiceApi api = new DataService();
-            Assert.AreEqual(api.getCustomers().Count(), 0);
+            IServiceApi api = new TestDataService();
+            api.addCustomer(1, "John", "Smiths");
+            api.addCustomer(2, "John", "Decker");
+            api.addCustomer(3, "John", "Stones");
+            Assert.AreEqual(api.getCustomerFirstName(1),  "John");;
         }
 
-        /*  [TestMethod]
-          public void TestMethod2()
-          {
-              IServiceApi api = new DataService();
-              api.addProduct(1, "Potato", 12, 300);
-              api.addProduct(2, "Tomato", 12, 32);
-              api.addProduct(3, "Cucumber", 12, 3300);
-              api.addProduct(4, "Milk", 12, 510);
-              Assert.AreEqual(api.getProductPrice(1) + api.getProductPrice(2), 24);
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IServiceApi api = new TestDataService();
+            api.addCustomer(1, "John", "Smiths");
+            api.addCustomer(2, "Jase", "Decker");
+            api.addCustomer(3, "John", "Stones");
+            Assert.AreEqual(api.getCustomerFirstName(1), "Jase"); ;
+        }
 
-          }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            IServiceApi api = new TestDataService();
+            api.addCustomer(1, "John", "Smiths");
+            api.addCustomer(2, "John", "Decker");
+            api.addCustomer(3, "John", "Stones");
+            Assert.AreEqual(api.getCustomerFirstName(1), "John"); ;
+        }
 
-          [TestMethod]
-          public void TestMethod3()
-          {
-              IServiceApi api = new DataService();
-              api.addEvent(1, 1, 1, "b");
-              api.addEvent(2, 1, 1, "s");
-              Assert.AreEqual(api.getEvents().Count(), 1);
-          }
-
-      }*/
     }
 }
